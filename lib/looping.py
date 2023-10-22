05520df69bf85d843ad7f1222115fba3b4a018d8
Tiana Lopez <tianadavin@gmail.com>
2023-10-22 13:02:53 -0700

    passes tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i > 0:
+        print(f"{i}")
+        i-=1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_list = list()
+    for i in int_list:
+        new_value = i ** 2
+        new_list.append(new_value)
+    return new_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
